ec81efb7a846110a4f1de65ae87b4e343b99f30e
siddu1257 <siddub1257@gmail.com>
2025-03-09 19:42:56 +0530

    first commit

---
+++ gateway-server/src/test/java/com/gk/gateway/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
